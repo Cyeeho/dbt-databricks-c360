44dc44501a4d3364083e5a24780cde09a29eb46a
Cyeeho <Cyeeho>
2023-11-27 13:44:42 +0000

    test 2

--- models/total_sales_by_user.sql
+++ models/total_sales_by_user.sql
@@ -2,4 +2,4 @@
  config(materialized = 'table', file_format = 'delta')
 }}
 
-Select user_id, sum(amount) from dbdemos.dbt_c360_bronze_orders group by user_id;
+Select user_id, sum(amount) as amount from dbdemos.dbt_c360_bronze_orders group by user_id;
